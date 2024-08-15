ecb2966aa81c40dcd66763c182425dcf5f24c13e
JahairaGR <105512694+JahairaGR@users.noreply.github.com>
2024-08-15 02:58:45 -0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,10 @@
-from flask_script import Manager, Server
 from app import inicializar_app
 from config import config
 
-configuracion=config['development']
-app= inicializar_app(configuracion)
-
-manager= Manager(app)
-manager.add_command('runserver',Server(host='127.0.0.1',port=5000))
+# Cargar la configuración
+configuracion = config['development']
+app = inicializar_app(configuracion)
 
 if __name__ == '__main__':
-    manager.run()
+    # Ejecutar la aplicación con el servidor integrado de Flask
+    app.run(host='127.0.0.1', port=5000)
